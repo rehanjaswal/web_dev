ef6763d157dfb1dbfb78baaef5f4762bd6ca4fa0
rehanjaswal <rehanxjaswal@gmail.com>
2026-02-18 22:53:00 +0530

    JS jargon

---
+++ JS/01_basics.js
@@ -0,0 +1,20 @@
+console.log("im gay")
+
+// JS is a dynamically typed language
+var a = 7
+a = "rehan"
+a = true
+console.log(a)  // logs true
+
+// single threaded language
+
+// prefer to use let more than var, use const when you know the value of that thing wont change throughout the code
+
+let x = 10
+console.log(x)
+
+let likePizza = true
+console.log("i like pizza. fact check status:",likePizza)
+
+let numbers = [1, 2, 3]
+console.log(numbers)
---
+++ JS/02_functions.js
@@ -0,0 +1,19 @@
+// let firstName = "rehan"
+// console.log("hello " + firstName)
+// console.log("kive aa singh " + firstName)
+// console.log("whats good my nigga " + firstName)
+
+// a better way to do this is to define a function which greets me
+
+function greet(nameOfPerson) {
+    console.log("kive aa singh " + nameOfPerson)
+    console.log("onichan " + nameOfPerson)
+}
+
+firstName1 = "rehan"
+firstName2 = "RJ"
+firstName3 = "jaswal"
+
+greet(firstName1)
+greet(firstName2)
+greet(firstName3)
