efe990b49e88948b4bc4ed7a28a830419fc4a5fd
rehanjaswal <rehanxjaswal@gmail.com>
2026-02-21 13:02:29 +0530

    insane arr concepts

--- JS/03_complex_types/a_little_complex_array.js
+++ JS/03_complex_types/a_little_complex_array.js
@@ -1,15 +1,51 @@
-// 
+// function isLegal(user) {
+//     if (user.age >= 18) console.log(user.name + " is allowed to vote")
+//     else console.log(user.name + " is not allowed to vote")
+// }
+
+// let users = [
+//     {
+//     name: "rehan",
+//     age: 19,
+//     address : [
+//         {
+//             city: "manchester",
+//             road: "sir matt busby road",
+//             place: "old trafford, greater manchester",
+//         }
+//     ]
+// },
+//     {
+//         name: "akshita",
+//         age: 19,
+//         address: [
+//             {
+//                 city: "zurich",
+//                 area: "Bahnhofstrasse",
+//                 pincode: "8001",
+//             }   
+//         ]  
+//     }
+//     ]
+
+// console.log(users[0].address[0].place)
+
+// for (let i = 0; i < users.length; i++) {
+//     isLegal(users[i])
+// }
+
+// users.forEach(isLegal)  // better and a cleaner way to do this
 
 
-// method 2 (returning a value)
+//  method 2
 
 function getLegalUsers(users) {
-    let legalUsers = []
+    let allowedUsers = [];
     for (let i = 0; i < users.length; i++) {
         if (users[i].age >= 18)
-            legalUsers.push(users[i])
+            allowedUsers.push(users[i]);
     }
-    return legalUsers
+    return allowedUsers;
 }
 
 let users = [
@@ -37,7 +73,12 @@ let users = [
     }
     ]
 
-let allowedUsers = getLegalUsers(users)
-console.log(allowedUsers)
-console.log(typeof allowedUsers)  // logs object, as an array is a specialised type of an object in JS
-console.log(Array.isArray(allowedUsers))  // if array -> returns true...else false
+// console.log(getLegalUsers(users))   // log directly if you dont wanna store value
+
+// if you wanna store its value, do this ->
+
+let legalUsers = getLegalUsers(users)
+console.log(legalUsers)
+
+console.log(legalUsers[1]) // useful for this kinda shit
+console.log(legalUsers.length)  // logs 2, as both users are legal
